c0abed38972af0ebeabc436013fadc49888a469d
abdelmouttalib <abdelmouttalib.jadid@gmail.com>
2018-09-24 14:38:53 +0200

    create module

--- formation/src/app/app.module.ts
+++ formation/src/app/app.module.ts
@@ -1,6 +1,10 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import { LoginModule } from './login/login.module';
+import { UiModule } from './ui/ui.module';
+import { PageNotFoundModule } from './page-not-found/page-not-found.module';
+
 import { AppComponent } from './app.component';
 
 @NgModule({
@@ -8,7 +12,10 @@ import { AppComponent } from './app.component';
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    UiModule,
+    LoginModule,
+    PageNotFoundModule
   ],
   providers: [],
   bootstrap: [AppComponent]
